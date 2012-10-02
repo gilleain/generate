d48b77173e5ab55d82dd5dd88b6ba516f1a8db01
maclean <gilleain.torrance@gmail.com>
2012-10-02 17:20:02 +0100

    Add a main method to the graph file diff

--- src/tools/GraphFileDiff.java
+++ src/tools/GraphFileDiff.java
@@ -42,5 +42,17 @@ public class GraphFileDiff {
 		readerB.close();
 		return difference;
 	}
+	
+	public static void main(String[] args) {
+	    if (args.length > 1) {
+	        try {
+                for (Graph g : GraphFileDiff.diff(args[0], args[1])) {
+                    System.out.println(g);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+	    }
+	}
 
 }
